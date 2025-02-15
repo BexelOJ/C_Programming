8cde768c693d3f34ee43ba08d69d8c00dc97d1ec
BexelOJ <bexelo.j@outlook.com>
2025-02-14 22:20:22 -0800

    dec2bin code added, note added

---
+++ C_02_2025/20250215_dec2Bin.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void decimalToBinary(int n) {
+    // Size of an integer in bytes
+    int size = sizeof(n) * 8;
+    
+    // Loop through each bit of the integer
+    for (int i = size - 1; i >= 0; i--) {
+        // Print 1 or 0 based on the value of the bit
+        if ((n >> i) & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
+}
+
+int main() {
+    int num;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    printf("Binary representation: ");
+    decimalToBinary(num);
+
+    return 0;
+}
+
