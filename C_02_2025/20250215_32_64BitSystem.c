c45efb410301919cb6b62294b7f004c1f143cc92
BexelOJ <bexelo.j@outlook.com>
2025-02-14 21:42:38 -0800

    32/64 code modified

--- C_02_2025/20250215_32_64BitSystem.c
+++ C_02_2025/20250215_32_64BitSystem.c
@@ -6,7 +6,9 @@ int c = 0;
 c=sizeof(void*);
 if(c==4)
 	printf("This is a 32 Bit System\n");
-if(c==8)
+else if(c==8)
 	printf("This is a 64 Bit System\n");
+else
+	printf("Unknown System");
 return 0;
 }
